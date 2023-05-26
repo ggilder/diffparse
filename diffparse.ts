3b6387274e7b0a7a2b8628097086b62b138fc565
Gabriel Gilder <gabriel@gabrielgilder.com>
2023-05-26 08:57:22 -0700

    ChatGPT attempts to handle deleted lines

--- diffparse.ts
+++ diffparse.ts
@@ -1,6 +1,7 @@
 interface FileDiff {
   filename: string;
   addedLines: number[];
+  deletedLines: number[];
 }
 
 function parseGitDiff(diffOutput: string): FileDiff[] {
@@ -9,6 +10,7 @@ function parseGitDiff(diffOutput: string): FileDiff[] {
 
   let currentFileDiff: FileDiff | undefined;
   let currentAddedLines: number[] = [];
+  let currentDeletedLines: number[] = [];
   let lastHeaderLine: string | undefined;
   let currentLineNumber: number = 0;
 
@@ -17,24 +19,32 @@ function parseGitDiff(diffOutput: string): FileDiff[] {
       // New file diff starts
       if (currentFileDiff) {
         currentFileDiff.addedLines = currentAddedLines;
+        currentFileDiff.deletedLines = currentDeletedLines;
         fileDiffs.push(currentFileDiff);
       }
 
       currentFileDiff = {
         filename: getFilenameFromDiffHeader(line),
         addedLines: [],
+        deletedLines: [],
       };
       currentAddedLines = [];
+      currentDeletedLines = [];
       lastHeaderLine = undefined;
       currentLineNumber = 0;
     } else if (line.startsWith('@@')) {
       // Header line
       lastHeaderLine = line;
-      const lineNumber = getStartingLineNumberFromHeaderLine(line);
-      currentLineNumber = lineNumber;
+      const { startingLineNumber, numDeletedLines } = getLineInfoFromHeaderLine(line);
+      currentLineNumber = startingLineNumber;
+      currentDeletedLines = generateLineNumbers(startingLineNumber, numDeletedLines);
     } else if (line.startsWith('+') && lastHeaderLine && currentLineNumber !== 0) {
       // Added line
       currentAddedLines.push(currentLineNumber);
+      currentLineNumber++;
+    } else if (line.startsWith('-') && lastHeaderLine && currentLineNumber !== 0) {
+      // Deleted line
+      currentDeletedLines.push(currentLineNumber);
       currentLineNumber++;
     } else if (!line.startsWith('-')) {
       // Context line
@@ -45,6 +55,7 @@ function parseGitDiff(diffOutput: string): FileDiff[] {
   // Add the last file diff
   if (currentFileDiff) {
     currentFileDiff.addedLines = currentAddedLines;
+    currentFileDiff.deletedLines = currentDeletedLines;
     fileDiffs.push(currentFileDiff);
   }
 
@@ -59,13 +70,24 @@ function getFilenameFromDiffHeader(header: string): string {
   return filename;
 }
 
-function getStartingLineNumberFromHeaderLine(line: string): number {
-  // Extract the starting line number from the header line
-  const matches = line.match(/\+(\d+)/);
-  if (matches && matches.length > 1) {
-    return parseInt(matches[1], 10);
+function getLineInfoFromHeaderLine(line: string): { startingLineNumber: number; numDeletedLines: number } {
+  // Extract the starting line number and number of deleted lines from the header line
+  const matches = line.match(/\-(\d+),?(\d+)? \+(\d+),?(\d+)? @@/);
+  if (matches && matches.length === 5) {
+    const startingLineNumber = parseInt(matches[3], 10);
+    const numDeletedLines = matches[4] ? parseInt(matches[4], 10) : 1;
+    return { startingLineNumber, numDeletedLines };
   }
-  return 0;
+  return { startingLineNumber: 0, numDeletedLines: 0 };
+}
+
+function generateLineNumbers(startingLineNumber: number, count: number): number[] {
+  // Generate an array of line numbers starting from the given startingLineNumber
+  const lineNumbers: number[] = [];
+  for (let i = startingLineNumber; i < startingLineNumber + count; i++) {
+    lineNumbers.push(i);
+  }
+  return lineNumbers;
 }
 
 // Example usage
@@ -76,10 +98,18 @@ index abcdefg..1234567 100644
 +++ b/file1.txt
 @@ -1,3 +1,4 @@
  This is line 1
- This is line 2
-+This is an added line
+-This is line 2
++This is a modified line
  This is line 3
  This is line 4
++This is an added line
+diff --git a/file2.txt b/file2.txt
+index abcdefg..1234567 100644
+--- a/file2.txt
++++ b/file2.txt
+@@ -1 +1 @@
+-This is line 1
++This is a modified line
 `;
 
 const fileDiffs = parseGitDiff(diffOutput);
